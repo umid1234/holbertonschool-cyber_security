8a8e468fb769002c771ac58423dc7cb52073e8cc
umid1234 <umidaso1357@gmail.com>
2026-01-16 15:28:34 +0400

    update

--- web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
+++ web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-
-# ARG 1: New Host header value
-# ARG 2: Target URL
-# ARG 3: Form data
-
-curl -s \
-     -X POST \
-     -H "Host: $1" \
-     -H "Content-Type: application/x-www-form-urlencoded" \
-     -d "$3" \
-     "$2"
+curl -s \ -X POST \ -H "Host: $1" \ -H "Content-Type: application/x-www-form-urlencoded"\ -d "$3"\ "$2"
 
